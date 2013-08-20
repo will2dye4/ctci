925d708bdc908da3207e9029adc3b4fcc3c134b7
William <will2dye4@mac.com>
2013-08-20 11:52:38 -0400

    Question 1.2.

---
+++ src/com/williamdye/ctci/module1/Question1_2.java
@@ -0,0 +1,57 @@
+package com.williamdye.ctci.module1;
+
+public class Question1_2
+{
+
+    /**
+     * TASK: Implement a function void reverse(char* str) in C or C++ which reverses a null-terminated string.
+     * Clearly, this is a Java implementation, but I have tried to make it as close to a C implementation as possible.
+     * The C implementation (as yet untested) is given as a comment below.
+     * @param args  an array of strings to reverse
+     *              The reversed strings will be printed to the console.
+     */
+    public static void main(String[] args)
+    {
+        for (String string : args) {
+            reverse(string.toCharArray());
+        }
+    }
+
+    private static void reverse(char[] str)
+    {
+        int length = str.length;
+        for (int i = 0, j = length - 1; i < j; i++, j--) {
+            char tmp = str[i];
+            str[i] = str[j];
+            str[j] = tmp;
+        }
+        println(new String(str));
+    }
+
+    private static void println(String message)
+    {
+        System.out.println(message);
+    }
+
+    /*
+    #include <stdio.h>
+    #include <string.h>
+
+    void reverse(char* str)
+    {
+        int length = strlen(str);
+        int i, j;
+        for (i = 0, j = length - 1; i < length; i++, j--) {
+            swap(str, i, j);
+        }
+        printf("%s\n", str);
+    }
+
+    void swap(char* str, const int i, const int j)
+    {
+        char tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+    }
+     */
+}
---
+++ test/com/williamdye/ctci/module1/Question1_2_Test.java
@@ -0,0 +1,37 @@
+package com.williamdye.ctci.module1;
+
+import org.junit.*;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class Question1_2_Test
+{
+    private static final String[] args = {"William", "ZYXWVUTS", "racecar", "7331", "<><><><><>", "1", "12"};
+    private static final String[] argsReversed = {"mailliW", "STUVWXYZ", "racecar", "1337", "><><><><><", "1", "21"};
+
+    private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeClass
+    public static void setUpOutputStream()
+    {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    public void invokeMain_PrintsReversedStrings()
+    {
+        Question1_2.main(args);
+        assertConsoleLinesEqualReversedArgs();
+    }
+
+    private void assertConsoleLinesEqualReversedArgs()
+    {
+        String[] consoleLines = outputStream.toString().split("\n");
+        for (int i = 0; i < argsReversed.length; i++) {
+            assertEquals(argsReversed[i], consoleLines[i]);
+        }
+    }
+
+}
