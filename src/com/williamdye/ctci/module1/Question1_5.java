694141792fa01c163ebd0e3308dea1826d2b430a
William <will2dye4@mac.com>
2013-08-23 22:35:10 -0400

    Adding Javadoc for Q1.5.

--- src/com/williamdye/ctci/module1/Question1_5.java
+++ src/com/williamdye/ctci/module1/Question1_5.java
@@ -2,6 +2,12 @@ package com.williamdye.ctci.module1;
 
 import com.williamdye.ctci.AbstractQuestion;
 
+/**
+ * TASK: Implement a method to perform basic string compression using the counts
+ * of repeated characters. For example, the string "aabcccccaaa" would become
+ * "a2b1c5a3". If the "compressed" string would not become smaller than the original
+ * string, your method should return the original string.
+ */
 public class Question1_5 extends AbstractQuestion
 {
 
